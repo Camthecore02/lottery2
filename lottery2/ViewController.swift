d99f24cf104ef827a770676547f4426a9aa3f1cc
Cameron Smith <smith.cam1278@yahoo.com>
2021-03-22 13:18:03 -0400

    Initial Commit

---
+++ lottery2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lottery2
+//
+//  Created by Cameron Smith on 3/22/21.
+//  Copyright © 2021 Cameron Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
